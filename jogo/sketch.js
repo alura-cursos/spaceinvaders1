5ecf09e7e7cb263da37b9123291a5e9e1cc1df63
bugan <dones.bugan@gmail.com>
2018-03-20 10:05:09 -0300

    video 1.2

--- jogo/sketch.js
+++ jogo/sketch.js
@@ -1,7 +1,23 @@
+let imagemNave;
+let imagemAlien;
+
+function preload(){
+// carregando imagens
+imagemNave = loadImage("imagens/Nave.png");
+imagemAlien = loadImage("imagens/Alien1.png");
+}
+
 function setup() {
-  // put setup code here
+// criando um palco
+createCanvas(900, 600);
 }
 
 function draw() {
-  // put drawing code here
+// pintar o fundo do palco
+background(100);
+
+//desenhar a nave
+image(imagemNave, 400, 500);
+image(imagemAlien, 400, 200);
+
 }

aab1fec7648133eb8ceeaec950d5402265ed38bf
bugan <dones.bugan@gmail.com>
2018-03-20 12:10:29 -0300

    video 1.3

--- jogo/sketch.js
+++ jogo/sketch.js
@@ -1,10 +1,17 @@
 let imagemNave;
 let imagemAlien;
+let imagemMisisl;
+
+let posicaoYMissil = 500;
+let posicaoXMissil = 400;
+let posicaoXNave;
+let posicaoYNave = 500;
 
 function preload(){
 // carregando imagens
 imagemNave = loadImage("imagens/Nave.png");
 imagemAlien = loadImage("imagens/Alien1.png");
+imagemMissil = loadImage("imagens/Missil.png");
 }
 
 function setup() {
@@ -16,9 +23,19 @@ function draw() {
 // pintar o fundo do palco
 background(100);
 
+posicaoYMissil = posicaoYMissil - 1;
+
+posicaoXNave = mouseX-imagemNave.width/2;
 //desenhar a nave
-image(imagemNave, mouseX-imagemNave.width/2, 500);
+image(imagemNave, posicaoXNave, posicaoYNave);
 //desenhando o alien
 image(imagemAlien, 400, 200);
+//desenhando o missil na posicao x =400 e y = 500
+image(imagemMissil, posicaoXMissil, posicaoYMissil);
 
+}
+
+function mousePressed(){
+posicaoYMissil = posicaoYNave;
+posicaoXMissil = posicaoXNave;
 }

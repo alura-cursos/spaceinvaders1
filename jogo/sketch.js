e4df8513ddd86a23e3baf4702d8e8dd4f4892efb
bugan <dones.bugan@gmail.com>
2018-03-21 09:19:15 -0300

    video 1.5

--- jogo/sketch.js
+++ jogo/sketch.js
@@ -1,41 +1,45 @@
 let imagemNave;
 let imagemAlien;
-let imagemMisisl;
+let imagemMissil;
 
 let posicaoYMissil = 500;
 let posicaoXMissil = 400;
 let posicaoXNave;
 let posicaoYNave = 500;
 
+//preparando o ambiente de trabalho
+//carrengado as fantasias do nosso jogo
 function preload(){
-// carregando imagens
-imagemNave = loadImage("imagens/Nave.png");
-imagemAlien = loadImage("imagens/Alien1.png");
-imagemMissil = loadImage("imagens/Missil.png");
+    // carregando imagens
+    imagemNave = loadImage("imagens/Nave.png");
+    imagemAlien = loadImage("imagens/Alien1.png");
+    imagemMissil = loadImage("imagens/Missil.png");
 }
 
+//"Ao clicar na bandeira verder"
 function setup() {
-// criando um palco
-createCanvas(900, 600);
+    // criando um palco com 900 de largura e 600 de altura
+    createCanvas(900, 600);
 }
 
+//desenhando nosso atores - igual ao bloco "sempre" do scracth
 function draw() {
-// pintar o fundo do palco
-background(100);
-
-posicaoYMissil = posicaoYMissil - 1;
-
-posicaoXNave = mouseX-imagemNave.width/2;
-//desenhar a nave
-image(imagemNave, posicaoXNave, posicaoYNave);
-//desenhando o alien
-image(imagemAlien, 400, 200);
-//desenhando o missil na posicao x =400 e y = 500
-image(imagemMissil, posicaoXMissil, posicaoYMissil);
+    // pintar o fundo do palco de cinza
+    background(100);
+    //movendo o míssil para cima
+    posicaoYMissil = posicaoYMissil - 1;
+    //centralizando a posição da nave
+    posicaoXNave = mouseX - imagemNave.width / 2;
+    //desenhar a nave
+    image(imagemNave, posicaoXNave, posicaoYNave);
+    //desenhando o alien
+    image(imagemAlien, 400, 200);
+    //desenhando o missil
+    image(imagemMissil, posicaoXMissil, posicaoYMissil);
 
 }
-
+//quando o mouse for pressionado
 function mousePressed(){
-posicaoYMissil = posicaoYNave;
-posicaoXMissil = posicaoXNave;
+    posicaoYMissil = posicaoYNave;
+    posicaoXMissil = mouseX - imagemMissil.width / 2;
 }
